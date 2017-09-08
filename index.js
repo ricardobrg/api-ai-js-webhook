470d7d2eed1ffbd5b63abce3c79c6eed87ce070c
ricardobrg <ricardo@brgweb.com.br>
2017-09-08 20:40:50 -0300

    minor bug fixes

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var http = require('http');
-var fs = require('fs');
-var connect = require('connect');
 var express = require('express');
-
 var app = express();
-app.get('/', function(req, res  ) {
-	response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-	res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-	res.send(JSON.stringify({ "speech": response, "displayText": response 
-	//"speech" is the spoken version of the response, "displayText" is the visual version
-  	}));
-    });
 
-var server = app.listen(app.get('port'), function(){
-        var host = server.address().address;
-        var port = server.address().port;
-        console.log("Example app listening at http://%s:%s", host, port);
-    });
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+        //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
